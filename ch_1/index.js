b71b1c0d4c48fa7fc57c3fa029790d90cbfa2dae
DarkMark1991 <amiran.ramishvili@gmail.com>
2017-01-09 15:57:30 +0400

    connect node.js to mongodb

--- ch_1/index.js
+++ ch_1/index.js
@@ -1,5 +1,32 @@
 var _ = require("underscore");
+var mongodb = require("mongodb");
 
-_.each([1,2,3], function(v){
-    console.log(v);
+var uri = "mongodb://localhost:27017/example";
+
+mongodb.MongoClient.connect(uri, function(err, db){
+    if(err){
+        console.log(err);
+        process.exit(1);
+    }
+
+    db.collection("sample").insert({hello: "there"}, function(err, res){
+        if(err){
+            console.log(err);
+            process.exit(1);
+        }
+
+        db.collection("sample").find().toArray(function(err, docs){
+            if(err){
+                console.log(err);
+                process.exit(1);
+            }
+
+            console.log("Found docs:");
+            docs.forEach(function(doc){
+                console.log(JSON.stringify(doc));
+            });
+
+            process.exit(0);
+        });
+    });
 });
